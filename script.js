3fc15f3527b7216a5951cb7aa324634a389b57e3
Marco <marco.chianese93@gmail.com>
2020-10-27 17:34:10 +0100

    configurazione parziale pulsante "next"

--- script.js
+++ script.js
@@ -3,7 +3,29 @@
 
 $(document).ready(function() {
 
+// Configuri il pulsante next
 
+$('.next').click( function() {
+    // ho la necessita di immagazzinare l'immagine attualmente mostrata per poter tenere conto dell'elemento da riprodurre in seguito
+    var immagineCorrente = $("img.active") ;
+    $(".img-box img").removeClass("active");
+    $(immagineCorrente).next().addClass("active");
+
+    
+    console.log(immagineCorrente);
+
+
+
+})
+
+
+
+
+//Configuro il pulsante previous
+
+$('.previous').click( function() {
+
+})
 
 
 });
